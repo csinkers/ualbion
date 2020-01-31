827fb7c36f467f9bf518f2bb1a319607ca0c1d24
Cam Sinclair <csinkers@gmail.com>
2020-01-31 05:54:08 -0500

    Tidy up

--- sr.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-copy /Y ..\ghidrasym\Debug\ghidrasym.dll ..\pathtools\installed\x86
-copy /Y ..\ghidrasym\Debug\ghidrasym.pdb ..\pathtools\installed\x86
-pushd albion_sr
-runjob ..\..\pathtools\installed\x86\windbg.exe SR-Main.exe
-popd

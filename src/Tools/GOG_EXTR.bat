5a7aaf59f5a45513e0821c54dacdf8b396fe29de
Benjamin <noreply@example.com>
2020-07-31 23:20:01 +0200

    Fix GOG_EXTR.bat, mount Albion game installation to D beforehand, various improvements and error handling

--- src/Tools/GOG_EXTR.bat
+++ src/Tools/GOG_EXTR.bat
@@ -1,33 +1,61 @@
 @echo off
-if not exist ualbion.sln goto badpath
 
-imgmount d game.ins -t iso -fs iso
-if errorlevel 0 goto mountok
-imgmount d ..\game.ins -t iso -fs iso
-:mountok
+if not exist "C:\src\ualbion.sln" goto c_dir_err
+goto c_dir_ok
 
-D:
-cd ALBION
-mkdir C:\ALBION
-mkdir C:\ALBION\DRIVERS
-mkdir C:\ALBION\SAVES
-mkdir C:\ALBION\CD
-mkdir C:\ALBION\CD\XLDLIBS
-mkdir C:\ALBION\CD\XLDLIBS\INITIAL
-mkdir C:\ALBION\CD\XLDLIBS\ENGLISH
-mkdir C:\ALBION\CD\XLDLIBS\GERMAN
-mkdir C:\ALBION\CD\XLDLIBS\FRENCH
-
-copy ROOT\MAIN.EXE C:\ALBION
-copy DRIVERS\ALBISND.OPL C:\ALBION\DRIVERS
-copy XLDLIBS\*.* C:\ALBION\CD\XLDLIBS
-copy XLDLIBS\INITIAL\*.* C:\ALBION\CD\XLDLIBS\INITIAL
-copy XLDLIBS\ENGLISH\*.* C:\ALBION\CD\XLDLIBS\ENGLISH
-copy XLDLIBS\GERMAN\*.* C:\ALBION\CD\XLDLIBS\GERMAN
-copy XLDLIBS\FRENCH\*.* C:\ALBION\CD\XLDLIBS\FRENCH
+:c_dir_err
+echo Error: C drive must be mounted to point at the ualbion main directory.
+echo E.g. mount C "C:\Git\ualbion"
 goto end
 
-:badpath
-echo C drive must be mounted to point at the ualbion depot directory.
+:c_dir_ok
+
+if not exist "D:\game.ins" goto d_dir_err
+if not exist "D:\game.gog" goto d_dir_err
+goto d_dir_ok
+
+:d_dir_err
+echo Error: D drive must be mounted to point at the Albion GOG installation.
+echo E.g. mount D "C:\Program Files\Albion"
+goto end
+
+:d_dir_ok
+
+imgmount E D:\game.ins -t iso -fs iso
+if errorlevel 1 goto mount_err
+if not exist E:\ALBION goto mount_err
+goto mount_ok
+
+:mount_err
+echo Error: Could not mount Albion CD image, please verify your game installation.
+goto end
+
+:mount_ok
+
+set DST=C:\ALBION
+mkdir %DST%
+mkdir %DST%\DRIVERS
+mkdir %DST%\SAVES
+mkdir %DST%\CD
+mkdir %DST%\CD\XLDLIBS
+mkdir %DST%\CD\XLDLIBS\INITIAL
+mkdir %DST%\CD\XLDLIBS\ENGLISH
+mkdir %DST%\CD\XLDLIBS\GERMAN
+mkdir %DST%\CD\XLDLIBS\FRENCH
+
+set SRC=E:\ALBION
+copy %SRC%\ROOT\MAIN.EXE %DST%
+copy %SRC%\DRIVERS\ALBISND.OPL %DST%\DRIVERS
+copy %SRC%\XLDLIBS\*.* %DST%\CD\XLDLIBS
+copy %SRC%\XLDLIBS\INITIAL\*.* %DST%\CD\XLDLIBS\INITIAL
+if exist %SRC%\XLDLIBS\ENGLISH copy %SRC%\XLDLIBS\ENGLISH\*.* %DST%\CD\XLDLIBS\ENGLISH
+if exist %SRC%\XLDLIBS\GERMAN copy %SRC%\XLDLIBS\GERMAN\*.* %DST%\CD\XLDLIBS\GERMAN
+if exist %SRC%\XLDLIBS\FRENCH copy %SRC%\XLDLIBS\FRENCH\*.* %DST%\CD\XLDLIBS\FRENCH
+
+if not exist %SRC%\XLDLIBS\ENGLISH echo Note: No English language files found.
+if not exist %SRC%\XLDLIBS\GERMAN echo Note: No German language files found.
+if not exist %SRC%\XLDLIBS\FRENCH echo Note: No French language files found.
+
+echo Successfully copied game file to ualbion.
 
 :end

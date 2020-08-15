646e25b1095e80780feee5a672b4b55f5466b18c
Cam Sinclair <csinkers@gmail.com>
2020-08-15 18:36:30 -0400

    Update dependency update script for Windows

--- src/get_dependencies.bat
+++ src/get_dependencies.bat
@@ -1,8 +1,27 @@
 @echo off
 pushd ..\deps
+if exist AdlMidi.NET goto l1
 git clone https://github.com/CSinkers/AdlMidi.NET
-:: Optional dependencies
-:: git clone https://github.com/CSinkers/SerdesNet
-:: git clone https://github.com/mellinoe/veldrid
-:: git clone https://github.com/mellinoe/veldrid-spirv
+:l1
+if exist SerdesNet goto l2
+git clone https://github.com/CSinkers/SerdesNet
+:l2
+if exist veldrid goto l3
+git clone https://github.com/mellinoe/veldrid
+:l3
+if exist veldrid-spirv goto l4
+git clone https://github.com/mellinoe/veldrid-spirv
+:l4
+pushd AdlMidi.NET
+git pull --rebase
+popd
+pushd SerdesNet
+git pull --rebase
+popd
+pushd veldrid
+git pull --rebase
+popd
+pushd veldrid-spirv
+git pull --rebase
+popd
 popd

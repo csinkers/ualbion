7709d176d232036a739623520006931f7c69bdb4
Cam Sinclair <csinkers@gmail.com>
2020-08-29 10:41:11 -0400

    Update dependency directories

--- src/get_dependencies.sh
+++ src/get_dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-pushd ../deps
-[ -d AdlMidi.NET ] || git clone https://github.com/CSinkers/AdlMidi.NET
-[ -d SerdesNet ] || git clone https://github.com/CSinkers/SerdesNet
-[ -d veldrid ] || git clone https://github.com/mellinoe/veldrid
+cd ../deps
+[ -d AdlMidi.NET ]   || git clone https://github.com/CSinkers/AdlMidi.NET
+[ -d SerdesNet ]     || git clone https://github.com/CSinkers/SerdesNet
+[ -d veldrid ]       || git clone https://github.com/mellinoe/veldrid
 [ -d veldrid-spriv ] || git clone https://github.com/mellinoe/veldrid-spirv
-pushd AdlMidi.NET; git pull --rebase; popd
-pushd SerdesNet; git pull --rebase; popd
-pushd veldrid; git pull --rebase; popd
-pushd veldrid-spirv; git pull --rebase; popd
-popd
+cd AdlMidi.NET;   git pull --rebase; cd ..
+cd SerdesNet;     git pull --rebase; cd ..
+cd veldrid;       git pull --rebase; cd ..
+cd veldrid-spirv; git pull --rebase; cd ..
+cd ..

6c3ffa5e8ea0ab444c3fe47daf94dfe5a657ba79
Cam Sinclair <csinkers@gmail.com>
2020-08-30 19:56:02 -0400

    Update dependencies and paths

--- src/get_dependencies.sh
+++ src/get_dependencies.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 cd ../deps
-[ -d AdlMidi.NET ]   || git clone https://github.com/CSinkers/AdlMidi.NET
-[ -d SerdesNet ]     || git clone https://github.com/CSinkers/SerdesNet
-[ -d veldrid ]       || git clone https://github.com/mellinoe/veldrid
+[ -d AdlMidi.NET ]   || git clone https://github.com/csinkers/AdlMidi.NET
+[ -d SerdesNet ]     || git clone https://github.com/csinkers/SerdesNet
+[ -d veldrid ]       || git clone https://github.com/csinkers/veldrid && git checkout -b docking origin/docking
 [ -d veldrid-spriv ] || git clone https://github.com/mellinoe/veldrid-spirv
+printf "\nUpdating AdlMidi.NET\n"
 cd AdlMidi.NET;   git pull --rebase; cd ..
+printf "\nUpdating SerdesNet\n"
 cd SerdesNet;     git pull --rebase; cd ..
+printf "\nUpdating veldrid\n"
 cd veldrid;       git pull --rebase; cd ..
+printf "\nUpdating veldrid-spirv\n"
 cd veldrid-spirv; git pull --rebase; cd ..
 cd ..

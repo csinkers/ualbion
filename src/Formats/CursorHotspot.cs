71ee534af1b665ab89c05f24b5413c5b0d9e454f
Cam Sinclair <csinkers@gmail.com>
2022-07-24 20:42:07 -0400

    Fix cursor hotpspot exception messages

--- src/Formats/CursorHotspot.cs
+++ src/Formats/CursorHotspot.cs
@@ -13,9 +13,9 @@ public class CursorHotspot
         if (string.IsNullOrWhiteSpace(s)) return null;
         var parts = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-        if (parts.Length != 2) throw new FormatException("Expected hotspot location to consist of two ints/floats separated by a space, but was \"{s}\"");
-        if (!float.TryParse(parts[0], out var x)) throw new FormatException("Expected hotspot location to consist of two ints/floats separated by a space, but was \"{s}\"");
-        if (!float.TryParse(parts[0], out var y)) throw new FormatException("Expected hotspot location to consist of two ints/floats separated by a space, but was \"{s}\"");
+        if (parts.Length != 2) throw new FormatException($"Expected hotspot location to consist of two ints/floats separated by a space, but was \"{s}\"");
+        if (!float.TryParse(parts[0], out var x)) throw new FormatException($"Expected hotspot location to consist of two ints/floats separated by a space, but was \"{s}\"");
+        if (!float.TryParse(parts[0], out var y)) throw new FormatException($"Expected hotspot location to consist of two ints/floats separated by a space, but was \"{s}\"");
 
         return new CursorHotspot { X = x, Y = y };
     }

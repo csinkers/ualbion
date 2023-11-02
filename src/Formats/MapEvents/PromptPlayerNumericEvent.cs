d68c5ab287c2b13af9ecbfc9d9855366b7f803af
Cam Sinclair <csinkers@gmail.com>
2023-11-02 07:06:11 +0100

    Async experiments

--- src/Formats/MapEvents/PromptPlayerNumericEvent.cs
+++ src/Formats/MapEvents/PromptPlayerNumericEvent.cs
@@ -8,28 +8,28 @@ namespace UAlbion.Formats.MapEvents;
 public class PromptPlayerNumericEvent : QueryEvent
 {
     public override QueryType QueryType => QueryType.PromptPlayerNumeric;
-    [EventPart("op")] public QueryOperation Operation { get; private set; } // method to use for check? 0,1,2,3,4,5
-    [EventPart("imm")] public byte Immediate { get; private set; } // immediate value?
-    [EventPart("src")] public ushort Argument { get; set; }
+    [EventPart("op")] public QueryOperation Operation { get; private set; }
+    [EventPart("src")] public ushort Argument { get; set; } // The value to match
     PromptPlayerNumericEvent() { }
-    public PromptPlayerNumericEvent(QueryOperation operation, byte immediate, ushort argument)
+    public PromptPlayerNumericEvent(QueryOperation operation, ushort argument)
     {
         Operation = operation;
-        Immediate = immediate;
         Argument = argument;
     }
     public static PromptPlayerNumericEvent Serdes(PromptPlayerNumericEvent e, ISerializer s)
     {
         if (s == null) throw new ArgumentNullException(nameof(s));
         e ??= new PromptPlayerNumericEvent();
-        e.Operation = s.EnumU8(nameof(Operation), e.Operation);
-        e.Immediate = s.UInt8(nameof(Immediate), e.Immediate);
-        int zeroes = s.UInt8(null, 0);
-        zeroes += s.UInt8(null, 0);
-        e.Argument = s.UInt16(nameof(Argument), e.Argument);
+        e.Operation = s.EnumU8(nameof(Operation), e.Operation); // 1
+
+        int zeroes = s.UInt8(null, 0); // 2
+        zeroes += s.UInt8(null, 0); // 3
+        zeroes += s.UInt8(null, 0); // 4
+
+        e.Argument = s.UInt16(nameof(Argument), e.Argument); // 6
         // field 8 is the next event id when the condition is false and is deserialised as part of the BranchEventNode that this event should belong to.
 
-        s.Assert(zeroes == 0, "PromptPlayerNumericEvent: Expected fields 3,4 to be 0");
+        s.Assert(zeroes == 0, "PromptPlayerNumericEvent: Expected fields 2,3,4 to be 0");
         return e;
     }
 }
---
+++ src/Tools/AsyncTests/DeterministicTaskScheduler.cs
@@ -0,0 +1,39 @@
+﻿namespace AsyncTests;
+
+/// <summary>
+/// TaskScheduler for executing tasks on the same thread that calls RunTasksUntilIdle() or RunPendingTasks().
+/// From https://github.com/dotnet/dotnet/blob/main/src/source-build-externals/src/application-insights/BASE/Test/TestFramework/Shared/DeterministicTaskScheduler.cs
+/// Copyright (c) 2015 Microsoft (MIT License https://github.com/dotnet/dotnet/blob/main/src/source-build-externals/src/application-insights/BASE/LICENSE)
+/// </summary>
+public class DeterministicTaskScheduler : TaskScheduler
+{
+    readonly List<Task> scheduledTasks = new();
+    public override int MaximumConcurrencyLevel => 1;
+
+    /// <summary>
+    /// Executes the scheduled Tasks synchronously on the current thread. If those tasks schedule new tasks
+    /// they will also be executed until no pending tasks are left.
+    /// </summary>
+    public void RunTasksUntilIdle()
+    {
+        while (scheduledTasks.Any())
+            RunPendingTasks();
+    }
+
+    /// <summary>
+    /// Executes the scheduled Tasks synchronously on the current thread. If those tasks schedule new tasks
+    /// they will only be executed with the next call to RunTasksUntilIdle() or RunPendingTasks(). 
+    /// </summary>
+    public void RunPendingTasks()
+    {
+        foreach (var task in scheduledTasks.ToArray())
+        {
+            TryExecuteTask(task);
+            scheduledTasks.Remove(task);
+        }
+    }
+
+    protected override void QueueTask(Task task) => scheduledTasks.Add(task);
+    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) => TryExecuteTask(task);
+    protected override IEnumerable<Task> GetScheduledTasks() => scheduledTasks;
+}

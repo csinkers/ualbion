8c06be8b6a2f54e9a72b68faef578bbfe7dfa977
Cam Sinclair <csinkers@gmail.com>
2023-12-31 09:28:58 +1000

    Async experiments (AlbionTask)

---
+++ src/Tools/AsyncTests/AlbionTask.cs
@@ -0,0 +1,127 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AsyncTests;
+/// <summary>
+/// A much simpler alternative to Task. Handles all async on the main thread,
+/// each frame any queued unblocked AlbionTasks will be progressed.
+/// </summary>
+[AsyncMethodBuilder(typeof(AlbionTaskBuilder))]
+public class AlbionTask : INotifyCompletion
+{
+    public static AlbionTask CompletedTask { get; } = new() { IsCompleted = true };
+    object? _continuation;
+    public AlbionTask GetAwaiter() => this;
+    public bool IsCompleted { get; private set; }
+
+    public void OnCompleted(Action continuation)
+    {
+        if (IsCompleted)
+        {
+            continuation();
+            return;
+        }
+
+        if (_continuation == null)
+        {
+            _continuation = continuation;
+            return;
+        }
+
+        if (_continuation is not List<Action> list)
+        {
+            list = new List<Action>();
+            _continuation = list;
+        }
+
+        list.Add(continuation);
+    }
+
+    internal void SetResult()
+    {
+        IsCompleted = true;
+        switch (_continuation)
+        {
+            case Action action:
+                action();
+                break;
+
+            case List<Action> list:
+            {
+                foreach (var action in list)
+                    action();
+                break;
+            }
+        }
+    }
+    public void GetResult()
+    {
+        if (!IsCompleted)
+            throw new InvalidOperationException("Tried to get result of an incomplete task");
+    }
+
+    public void Start() => AlbionTaskScheduler.Default.QueueTask(this);
+}
+public class AlbionTask<T> : INotifyCompletion
+{
+    object? _continuation;
+    T? _result;
+    public static AlbionTask<T> FromResult(T value) =>
+        new()
+        {
+            _result = value,
+            IsCompleted = true
+        };
+
+    public AlbionTask<T> GetAwaiter() => this;
+    public bool IsCompleted { get; private set; }
+    public void OnCompleted(Action continuation)
+    {
+        if (IsCompleted)
+        {
+            continuation();
+            return;
+        }
+
+        if (_continuation == null)
+        {
+            _continuation = continuation;
+            return;
+        }
+
+        if (_continuation is not List<Action> list)
+        {
+            list = new List<Action>();
+            _continuation = list;
+        }
+
+        list.Add(continuation);
+    }
+
+    public T GetResult()
+    {
+        if (!IsCompleted)
+            throw new InvalidOperationException("Tried to get result of an incomplete task");
+
+        return _result!;
+    }
+
+    internal void SetResult(T value)
+    {
+        _result = value;
+        IsCompleted = true;
+
+        switch (_continuation)
+        {
+            case Action action:
+                action();
+                break;
+
+            case List<Action> list:
+            {
+                foreach (var action in list)
+                    action();
+                break;
+            }
+        }
+    }
+}
---
+++ src/Tools/AsyncTests/AlbionTaskScheduler.cs
@@ -0,0 +1,34 @@
+﻿namespace AsyncTests;
+
+public class AlbionTaskScheduler
+{
+    public static AlbionTaskScheduler Default { get; } = new();
+    readonly List<AlbionTask> scheduledTasks = new();
+
+    /// <summary>
+    /// Executes the scheduled Tasks synchronously on the current thread. If those tasks schedule new tasks
+    /// they will also be executed until no pending tasks are left.
+    /// </summary>
+    public void RunTasksUntilIdle()
+    {
+        while (scheduledTasks.Any())
+            RunPendingTasks();
+    }
+
+    /// <summary>
+    /// Executes the scheduled Tasks synchronously on the current thread. If those tasks schedule new tasks
+    /// they will only be executed with the next call to RunTasksUntilIdle() or RunPendingTasks(). 
+    /// </summary>
+    public void RunPendingTasks()
+    {
+        foreach (var task in scheduledTasks.ToArray())
+        {
+            Console.WriteLine("ExecuteTask");
+            // task.???
+            scheduledTasks.Remove(task);
+        }
+    }
+
+    public void QueueTask(AlbionTask task) => scheduledTasks.Add(task);
+    protected IEnumerable<AlbionTask> GetScheduledTasks() => scheduledTasks;
+}

3af72047feb674077a82b0ba8c85f17ca5092c94
Cam Sinclair <csinkers@gmail.com>
2025-09-06 02:04:31 +1000

    Fix broken keybinds when NumLock enabled

--- src/Game.Veldrid/Input/KeyBinding.cs
+++ src/Game.Veldrid/Input/KeyBinding.cs
@@ -7,7 +7,15 @@ public struct KeyBinding : IEquatable<KeyBinding>
 {
     public Key Key { get; }
     public ModifierKeys Modifiers { get; }
-    public KeyBinding(Key key, ModifierKeys modifiers) { Key = key; Modifiers = modifiers; }
+    public KeyBinding(Key key, ModifierKeys modifiers) { Key = key; Modifiers = FilterModifiers(modifiers); }
+
+    static ModifierKeys FilterModifiers(ModifierKeys modifiers) =>
+        modifiers & (ModifierKeys.LeftShift
+            | ModifierKeys.RightShift
+            | ModifierKeys.LeftControl
+            | ModifierKeys.RightControl
+            | ModifierKeys.LeftAlt
+            | ModifierKeys.RightAlt);
 
     public bool Equals(KeyBinding other) { return Key == other.Key && Modifiers == other.Modifiers; }
     public override bool Equals(object obj) { return obj is KeyBinding other && Equals(other); }
